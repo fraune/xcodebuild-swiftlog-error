93a3037116eea7b0923367f26514f42f6668db42
Brandon Fraune <brandon.fraune@gmail.com>
2022-09-16 11:28:38 -0500

    initial commit for basic app with swift-log xcodebuild error

---
+++ AppDemo/AppDemo/AppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDemoApp.swift
+//  AppDemo
+//
+//  Created by Brandon Fraune on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct AppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
